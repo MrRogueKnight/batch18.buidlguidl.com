60dc062b25ecc1a1ded556e4baeb66e19e1bbc2a
MrRogueKnight <89287357+MrRogueKnight@users.noreply.github.com>
2025-07-14 23:24:33 +0530

    Update index.tsx

--- packages/nextjs/components/scaffold-eth/RainbowKitCustomConnectButton/index.tsx
+++ packages/nextjs/components/scaffold-eth/RainbowKitCustomConnectButton/index.tsx
@@ -9,9 +9,9 @@ import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { Address } from "viem";
 import { useAccount } from "wagmi";
 import { useNetworkColor } from "~~/hooks/scaffold-eth";
+import { useScaffoldReadContract } from "~~/hooks/scaffold-eth";
 import { useTargetNetwork } from "~~/hooks/scaffold-eth/useTargetNetwork";
 import { getBlockExplorerAddressLink } from "~~/utils/scaffold-eth";
-import { useScaffoldReadContract } from "~~/hooks/scaffold-eth";
 
 /**
  * Custom Wagmi Connect Button (watch balance + custom design)
