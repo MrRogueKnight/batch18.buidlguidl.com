b70b8fc395fa3ea7d87daa758673734d86e078b6
Animesh <animeshparashar2468@gmail.com>
2025-07-19 20:32:00 +0530

    addressed the changes

--- packages/nextjs/app/layout.tsx
+++ packages/nextjs/app/layout.tsx
@@ -5,7 +5,7 @@ import "~~/styles/globals.css";
 import { getMetadata } from "~~/utils/scaffold-eth/getMetadata";
 
 export const metadata = getMetadata({
-  title: "Home | Batch 18",
+  title: "Scaffold-ETH 2 App",
   description: "Built with 🏗 Scaffold-ETH 2",
 });
 
